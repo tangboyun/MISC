c00c53c2bac833c10b241d6c4723b23b98e8f811
Boyun Tang <tangboyun@hotmail.com>
2012-04-26 14:50:52 +0800

    complete the definition rvmTTest and normal ttest. TODO : testcase

--- PValueAdj/RVM.hs
+++ PValueAdj/RVM.hs
@@ -12,18 +12,83 @@
 -----------------------------------------------------------------------------
 module RVM where
 
-import Statistics.Distribution.Gamma
-import qualified Data.Vector.Unboxed as UV
-import Data.Packed.Matrix
+import Numeric.LinearAlgebra
+import Numeric.Container
+import qualified Data.Vector as V
+import Control.Exception
+import Numeric.GSL.Distribution.Continuous
+import Numeric.GSL
 
 type FloatType = Double 
+type ExpData = V.Vector (Vector FloatType)
 
-designMatrix :: [Int] -> Matrix FloatType
+data TestType = TwoTail 
+              | LeftTail
+              | RightTail  
+              deriving (Eq,Show)
+         
+designMatrix :: [Int] -> Matrix FloatType          
 designMatrix ns = 
   let l = length ns
   in fromLists $ 
-     concatMap (\(row,n) -> 
-           replicate n (replicate row 0 ++ 
-                        [1.0] ++ 
-                        replicate (l - row - 1) 0)
-          ) $ zip [0..] ns
+     concatMap 
+     (\(row,n) -> 
+       replicate n (replicate row 0 ++ 
+                    [1.0] ++ 
+                    replicate (l - row - 1) 0)
+     ) $ zip [0..] ns
+  
+getPfromT :: Vector FloatType -> FloatType -> TestType -> Vector FloatType
+getPfromT tV df tp =
+  let pf =
+        case tp of
+          TwoTail -> (\e -> if e > 1 then 1 else e ) . 
+                     (2 *) . 
+                     density_1p TDist Lower df . 
+                     negate . abs
+          LeftTail -> (\e -> if e > 1 then 1 else e ) .
+                      density_1p TDist Lower df . 
+                      negate
+          RightTail -> (\e -> if e > 1 then 1 else e ) .
+                       density_1p TDist Lower df 
+  in cmap pf tV
+
+rvmTTest :: ExpData -> ExpData -> TestType -> Vector FloatType
+rvmTTest group1 group2 tp = getPfromT tCorrected (fromIntegral df) tp
+  where                        
+    n1 = V.length group1
+    n2 = V.length group2
+    df = n1 + n2 - 2
+    stepSize = 0.1
+    precision = 1e-6
+    maxIter = 1000
+    startP = [1.0,1.0]
+    resSum = 
+      let x = designMatrix [n1,n2]
+          y = fromColumns $ V.toList group1 ++ V.toList group2
+          betaV = pinv (trans x <> x) <> trans x <> y
+          r = y `sub` x <> betaV 
+      in fromList $ 
+         map (foldVector (+) 0) $ 
+         toRows $ r `mul` r
+    estVar = cmap (/ fromIntegral df) resSum
+    fitF a b = -- a*b*varFit ~ F((n-2),2a) 
+      let v = a * b
+          f = negate . (+ log v) . log . 
+              density_2p FDist Density (fromIntegral df) (2*a) . ( * v)
+      in -- sum(-(log(fpdf(varFit*(arg(1)*arg(2)),df,2*arg(1)))+log(arg(1)*arg(2))));
+       foldVector (+) 0 $ cmap f estVar 
+    calcPartialDeriv [a,b] = 
+      let derivA = fst $ derivCentral stepSize (flip fitF b) a 
+          derivB = fst $ derivCentral stepSize (fitF a) b
+      in [derivA,derivB]
+    fWrap [a,b] = fitF a b
+    [estA,estB] = fst $ 
+                  minimizeD VectorBFGS2 
+                  precision maxIter stepSize 
+                  precision fWrap calcPartialDeriv startP
+    varCorrected = cmap ((/ (fromIntegral df + 2 * estA)) . (+ 1 / estB)) resSum
+    vecMean = map (\e -> foldVector (+) 0 e / fromIntegral (dim e)) . V.toList
+    tCorrected = zipVectorWith (/)
+                 (fromList $ zipWith (-) (vecMean group1) (vecMean group2)) 
+                 (cmap (sqrt . (* (1 / fromIntegral n1 + 1 / fromIntegral n2))) varCorrected)
---
+++ PValueAdj/Test.hs
@@ -0,0 +1,39 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module : 
+-- Copyright : (c) 2012 Boyun Tang
+-- License : BSD-style
+-- Maintainer : tangboyun@hotmail.com
+-- Stability : experimental
+-- Portability : ghc
+--
+-- 
+--
+-----------------------------------------------------------------------------
+module Main where
+
+import RVM
+import Statistics.Basic
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as UV
+import Data.Packed
+
+
+main :: IO ()
+main = undefined
+
+tTest :: ExpData -> ExpData -> TestType -> Vector FloatType
+tTest group1 group2 tp = getPfromT (fromList $ UV.toList t) df tp
+  where
+    n1 = fromIntegral $ V.length group1
+    n2 = fromIntegral $ V.length group2
+    df = n1 + n2 - 2
+    g1 = V.map (UV.fromList . toList) group1
+    g2 = V.map (UV.fromList . toList) group2
+    (m1,v1) = vecFastMeanVar g1
+    (m2,v2) = vecFastMeanVar g2
+    t = UV.zipWith (/) (UV.zipWith (-) m1 m2)
+        (UV.map (sqrt . (* (1/n1 + 1/n2)) . (/ df)) $ 
+         UV.zipWith (+) 
+         (UV.map (* (n1 - 1)) v1)
+         (UV.map (* (n2 - 1)) v2))

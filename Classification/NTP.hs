faebe4053241ab1df30cf6e42d654286568095d3
Boyun Tang <tangboyun@hotmail.com>
2012-06-27 10:31:18 +0800

    add permutation test

--- Classification/NTP.hs
+++ Classification/NTP.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module : Nearest Template Prediction
@@ -12,23 +13,32 @@
 -- 
 --
 -----------------------------------------------------------------------------
-module Classification.NTP 
+module NTP 
        (
-         nearestTemplatePrediction
+         ntp
+       , ntpWithPermTest
+       , ntpPredict
        )
        where
 
-import qualified Data.Vector.Unboxed as UV
+import           Control.Parallel.Strategies
+import           Data.List
+import qualified Data.Vector                 as V
+import qualified Data.Vector.Unboxed         as UV
+import           Sampling
+import           System.Random.MWC
+import           Types
+import PValueAdj
+import Debug.Trace
 
-type FloatType = Double
 data P3 = P {-# UNPACK #-} !FloatType
             {-# UNPACK #-} !FloatType
             {-# UNPACK #-} !FloatType
 
 {-# INLINE cosSim #-}
 cosSim :: UV.Vector FloatType -> UV.Vector FloatType -> FloatType
-cosSim v1 v2 = 
-  let P nu de1 de2 =
+cosSim !v1 !v2 = 
+  let !(P nu de1 de2) =
         UV.foldl' 
         (\(P n1 d1 d2) (x1,x2) -> 
           let n1' = n1 + x1*x2 
@@ -38,19 +48,83 @@ cosSim v1 v2 =
         ) (P 0 0 0) $ UV.zip v1 v2
   in nu / sqrt (de1 * de2)
 
+
 {-# INLINE cosDis #-}
 cosDis :: UV.Vector FloatType -> UV.Vector FloatType -> FloatType
 cosDis = ((1 -) .) . cosSim
 
-{-# INLINE nearestTemplatePrediction #-}
-nearestTemplatePrediction :: UV.Vector FloatType -> UV.Vector FloatType -> UV.Vector FloatType -> (Int,(FloatType,FloatType))
-nearestTemplatePrediction template weight sample = 
-  let sample' = UV.zipWith (*) sample weight
-      posTemplate = template
-      negTemplate = UV.map negate template
-      dis1 = cosDis sample' posTemplate
-      dis2 = cosDis sample' negTemplate
-      label = if dis1 < dis2 
-              then 1
-              else -1
+{-# INLINE ntp #-}
+ntp :: UV.Vector FloatType -- ^ Cox Score in this case
+    -> UV.Vector FloatType -- ^ Normalized expression value
+    -> (Int,(FloatType,FloatType)) -- ^ label and dis to each template, +1: close to posTem -1: close to negTem
+ntp !weight !sample = 
+  let !sample' = UV.zipWith (*) sample $ UV.map abs weight
+      !posTemplate = weight
+      !negTemplate = UV.map negate posTemplate
+      !dis1 = cosDis sample' posTemplate
+      !dis2 = cosDis sample' negTemplate
+      !label = if dis1 < dis2 
+               then 1
+               else -1
   in (label,(dis1,dis2))
+
+
+{-# INLINE ntpWithPermTest #-}
+ntpWithPermTest :: Int   -- ^ repeat times for permutatiion test
+                -> Seed  -- ^ initial random seed
+                -> UV.Vector Int -- ^ indices for selected signature
+                -> UV.Vector FloatType -- ^ weight for all genes
+                -> UV.Vector FloatType -- ^ exp data for all genes
+                -> ((Int,FloatType),Seed) -- ^ label with nominal p-value
+ntpWithPermTest !n !seed !idxVec !w !sample =
+  let sample' = UV.unsafeBackpermute sample idxVec -- signature
+      w' = UV.unsafeBackpermute w idxVec           -- sig's cox
+      (!label,!(dis1,dis2)) = ntp w' sample'
+      (!rnd_idx,!seed') = 
+        foldl' 
+        (\(acc,s) _ ->
+          let !(v,s') = shuffle s $ UV.enumFromN 0 $ 
+                        UV.length sample
+              !ls = UV.take (UV.length idxVec) v : acc
+          in (ls,s'))
+        ([],seed) [0..n-1]
+      rand_dis = UV.fromList $ 
+                 parMap rdeepseq
+                 (\idxV ->
+                   let !sam = UV.unsafeBackpermute sample idxV -- random genes
+                       !d = if label == 1
+                            then cosDis w' sam  -- Pos sample compare with pos template
+                            else cosDis (UV.map negate w') sam
+                   in d) rnd_idx
+      dis = if label == 1
+            then dis1
+            else dis2
+      !p = (UV.head $! rankAll $ dis `UV.cons` rand_dis) / fromIntegral (UV.length rand_dis + 1) 
+  in ((label,p),seed')
+
+ntpPredict :: FloatType -- ^ FDR control
+           -> Int       -- ^ repeat times for permutation test
+           -> Seed      -- ^ initial random seed
+           -> UV.Vector Int  -- ^ indices for selected signature
+           -> UV.Vector FloatType -- ^ weight for all genes
+           -> ExpData             -- ^ exp data for unlabeled sample
+           -> ((UV.Vector Int,UV.Vector Int),Seed) -- ^ true pos idx, neg idx
+ntpPredict !fdrCutOff !nPerm !seed !idxVec !w !normalizedExpData =
+  let (!labelInfos,seed') = V.foldl'
+                           (\(acc,s) sample ->
+                             let !((l,p),s') = ntpWithPermTest nPerm s idxVec w sample
+                                 !acc' = acc ++ [(l,p)]
+                             in (acc',s')
+                           ) ([],seed) normalizedExpData
+      (!labels,!ps) = unzip labelInfos
+      !lVec = UV.fromList labels
+      !pVec = UV.fromList ps
+      !posIdxs = UV.findIndices (== 1) lVec
+      -- !truePosIdxs = UV.findIndices (< fdrCutOff) $
+      --                bhFDR $ UV.unsafeBackpermute pVec posIdxs
+      
+      !truePosIdxs = bhFDR' fdrCutOff $ UV.unsafeBackpermute pVec posIdxs
+--      !negIdxs = UV.fromList $ [0..length labels - 1] \\ (UV.toList truePosIdxs)
+      !negIdxs = UV.fromList $ [0..length labels - 1] \\ (UV.toList posIdxs)
+  in trace (show $ bhFDR $ UV.fromList $ sort $ UV.toList $ UV.unsafeBackpermute pVec posIdxs) --((truePosIdxs,negIdxs),seed')
+     ((posIdxs,negIdxs),seed')

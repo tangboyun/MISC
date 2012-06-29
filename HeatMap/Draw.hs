a7ccc0ff11679861746253ee570100fa3b060bdf
Boyun Tang <tangboyun@hotmail.com>
2012-06-29 16:33:17 +0800

    add more options for displaying label

--- HeatMap/Draw.hs
+++ HeatMap/Draw.hs
@@ -17,6 +17,52 @@ import qualified Data.Vector.Unboxed as UV
 import qualified Data.Vector as V
 import Text.StringTemplate
 
+
+drawMatrixWithLabel :: Double -> Double -> String -> String -> String -> V.Vector (UV.Vector Double) -> (V.Vector String,String) -> (V.Vector String,String) -> Double -> Double -> String
+drawMatrixWithLabel width height posColor negColor bgColor m (rowNames,rowNameSize) (colNames,colNameSize) lineLen angle =
+  let nRow = V.length m
+      nCol = UV.length $ m `V.unsafeIndex` 0
+      w = width / fromIntegral nCol
+      h = height / fromIntegral nRow
+      rects = concatMap
+              (\(i,row) ->
+                map
+                (\(j,v) ->
+                  rectangle posColor negColor bgColor w h v (w * fromIntegral j,-h * fromIntegral i)) $
+                UV.toList $ UV.indexed row) $
+              V.toList $ V.indexed m
+      rowNameCoor = map (\(l,(x,y)) -> show x ++ "/" ++ show y ++ "/" ++ l) $
+                    zip (V.toList rowNames) $
+                    map (\idx -> (width, negate $ (fromIntegral idx + 0.5) * h)) [0..nRow-1]
+      colNameCoor = map (\(l,(x,y)) -> show x ++ "/" ++ show y ++ "/" ++ l) $
+                    zip (V.toList colNames) $
+                    map (\idx -> ((fromIntegral idx + 0.5) * w,0)) [0..nCol-1]
+      label = render $ setManyAttrib
+              [("rowTuples",rowNameCoor)
+              ,("colTuples",colNameCoor)
+              ,("lineLen", [show lineLen])
+              ,("halfLineLen",[show (lineLen/2)])
+              ,("angle",[show angle])
+              ,("colNameSize",[colNameSize])
+              ,("rowNameSize",[rowNameSize])
+              ] labelTemplate
+  in render $ setManyAttrib
+              [("rectangles",rects)
+              ,("labels",[label])
+              ] texTemplate
+
+
+labelTemplate :: StringTemplate String
+labelTemplate = newAngleSTMP
+  "\\\\foreach \\\\x/\\\\y/\\\\z in {<rowTuples;separator=','>}{\n\
+  \\\\\draw (\\\\x,\\\\y) --++(<halfLineLen>,0) node [right,font=\\\\<rowNameSize>] {\\\\z};\n\
+  \}\n\
+  \\\\\foreach \\\\x/\\\\y/\\\\z in {<colTuples;separator=','>}{\n\
+  \\\\\draw[] (\\\\x,\\\\y) -- ++(0,<halfLineLen>);\n\
+  \\\\\draw ($(\\\\x,\\\\y) + (0,<halfLineLen>) + (<angle>:0)$) -- ++(<angle>:<lineLen>) node [right,rotate=<angle>,font=\\\\<colNameSize>] {\\\\z};\n\
+  \}\n"
+
+
 drawMatrix :: Double -> Double -> String -> String -> String -> V.Vector (UV.Vector Double) -> String
 drawMatrix width height posColor negColor bgColor m =
   let nRow = V.length m
@@ -57,8 +103,10 @@ texTemplate = newSTMP
   "\\\\documentclass{standalone}\n\
    \\\\\usepackage{graphicx}\n\
    \\\\\usepackage{tikz}\n\
+   \\\\\usetikzlibrary{calc}\n\
    \\\\\begin{document}\n\
    \\\\\begin{tikzpicture}\n\
    \$rectangles;separator='\n'$\n\
+   \$labels$\n\
    \\\\\end{tikzpicture}\n\
    \\\\\end{document}\n"            

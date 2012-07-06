e977289a210a89c231a04143e3e7ff04ccefd3f2
Boyun Tang <tangboyun@hotmail.com>
2012-07-06 13:40:52 +0800

    add functions for plot hierarchical clustering dendro

--- HeatMap/Draw.hs
+++ HeatMap/Draw.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module : 
@@ -17,9 +18,11 @@ import qualified Data.Vector.Unboxed as UV
 import qualified Data.Vector as V
 import Text.StringTemplate
 import Data.Clustering.Hierarchical
-
-
-drawMatrixWithLabel :: Double -> Double -> String -> String -> String -> V.Vector (UV.Vector Double) -> (V.Vector String,String) -> (V.Vector String,String) -> Double -> Double -> String
+import Control.Arrow
+import qualified Data.Map as M
+import Data.List
+drawMatrixWithLabel :: Double -> Double -> String -> String -> String
+                    -> V.Vector (UV.Vector Double) -> (V.Vector String,String) -> (V.Vector String,String) -> Double -> Double -> String
 drawMatrixWithLabel width height posColor negColor bgColor m (rowNames,rowNameSize) (colNames,colNameSize) lineLen angle =
   let nRow = V.length m
       nCol = UV.length $ m `V.unsafeIndex` 0
@@ -110,5 +113,94 @@ texTemplate = newSTMP
    \\\\\begin{tikzpicture}\n\
    \$rectangles;separator='\n'$\n\
    \$labels$\n\
+   \$dendro$\n\
    \\\\\end{tikzpicture}\n\
    \\\\\end{document}\n"            
+
+
+
+data Axis = X
+          | Y
+          deriving (Eq)
+data Direct = Pos
+            | Neg
+            deriving (Eq)
+-- | Borrowed from Diagrams.Dendrogram
+fixedWidth :: Double -> Dendrogram a -> (Dendrogram (a, Double), Double)
+fixedWidth w = second (subtract half_w) . go half_w
+    where
+      half_w = w/2
+      go !y (Leaf datum) = (Leaf (datum, y), y + w)
+      go !y (Branch d l r) = (Branch d l' r', y'')
+          where
+            (l', !y')  = go y  l
+            (r', !y'') = go y' r
+
+deepth :: Dendrogram a -> Double
+deepth dendro = deepth' 0 dendro
+  where
+    deepth' acc (Leaf _) = acc
+    deepth' acc (Branch d l r) = d + max (deepth l) (deepth r)
+
+
+transDendro :: Dendrogram a -> Dendrogram a
+transDendro l@(Leaf _) = l
+transDendro (Branch d' l r) = Branch (d' + max (deepth l) (deepth r)) (transDendro l) (transDendro r)
+
+drawDendro :: Ord a => (Axis,Direct) -> (Double,Double) -> Double -> Double -> Dendrogram a -> String
+drawDendro (axis,direct) (x,y) w scale dendro = 
+  let dendro' = fst $ fixedWidth w dendro
+  in go dendro'
+  where
+    mean ls = let (sum,l) = foldl' (\(acc,len) e -> (acc+e,len+1)) (0,0) ls
+              in sum / l
+    go (Leaf _) = ""
+    go (Branch d l r) =
+      let wL = mean $ map snd $ elements l
+          hL = scale * deepth l
+          wR = mean $ map snd $ elements r
+          hR = scale * deepth r
+          d' = scale * d
+      in case axis of
+        X ->
+          let coordL = (x+wL,y+hL)
+              coordL' = if hL == 0 -- leaf
+                        then (x+wL,y+hL+d'+hR)
+                        else (x+wL,y+hL+d')
+              coordR = (x+wR,y+hR)
+              coordR' = if hR == 0 -- leaf
+                        then (x+wR,y+hR+d'+hL)
+                        else (x+wR,y+hR+d')
+              mapY = if direct == Pos
+                     then id
+                     else negY
+              negY (x',y') = (x',-y')
+          in "\\draw " ++ show (mapY coordL) ++ " -- " ++ show (mapY coordL') ++
+             " -- " ++ show (mapY coordR') ++ " -- " ++ show (mapY coordR) ++ ";\n" ++
+             go l ++ go r
+        Y ->
+          let coordL = (x+hL,y+wL)
+              coordL' = if hL == 0 -- leaf
+                        then (x+hL+d'+hR,y+wL)
+                        else (x+hL+d',y+wL)
+              coordR = (x+hR,y+wR)
+              coordR' = if hR == 0 -- leaf
+                        then (x+hR+d'+hL,y+wR)
+                        else (x+hR+d',y+wR)
+              mapX = if direct == Pos
+                     then id
+                     else negX
+              negX (x',y') = (-x',y')
+              
+          in "\\draw " ++ show (mapX coordL) ++ " -- " ++ show (mapX coordL') ++
+             " -- " ++ show (mapX coordR') ++ " -- " ++ show (mapX coordR) ++ ";\n" ++
+             go l ++ go r
+
+  
+testDendro = Branch 0.8
+             (Branch 0.5
+              (Branch 0.2
+               (Leaf 'A')
+               (Leaf 'B'))
+              (Leaf 'C'))
+             (Leaf 'D')

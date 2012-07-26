f4e4f84a02ea43758ce93ecc9935226b526b1278
Boyun Tang <tangboyun@hotmail.com>
2012-07-26 16:47:58 +0800

    add ttest func

--- PValueAdj/PAdj.hs
+++ PValueAdj/PAdj.hs
@@ -9,8 +9,9 @@
 -- Portability : ghc
 -- Reference : Storey JD. (2002) A direct approach to false discovery rates.
 --             Journal of the Royal Statistical Society, Series B, 64: 479-498.
--- 
 --
+--             Mitchell Guttman (2011) lincRNAs act in the circuitry controlling pluripotency and differentiation 
+--             Nature 477, 295–300 
 -----------------------------------------------------------------------------
 
 module PValueAdj where
@@ -18,11 +19,15 @@ module PValueAdj where
 import           Data.Function
 import           Data.List
 import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector as V
 import           Statistics.Distribution
 import           Statistics.Distribution.ChiSquared
 import System.Random.MWC
 import Control.Monad.ST
 import Debug.Trace
+import Statistics.Basic
+import Statistics.Distribution.StudentT
+import Statistics.Distribution
 
 type FloatType = Double
 
@@ -112,3 +117,55 @@ qValue seed lambda' pVec =
       myPi = min 1 $ pi0s `UV.unsafeIndex` piIdx                                 
       v = rankAll pVec
   in trace (show myPi ++ show pi0s) $ (UV.map ((*myPi).(* fromIntegral m)) $ UV.zipWith (/) pVec v,seed')
+
+
+tstat :: V.Vector (UV.Vector FloatType)
+      -> UV.Vector Bool
+      -> UV.Vector FloatType
+tstat !expData !label =
+  let idx1 = UV.findIndices id label
+      idx2 = UV.findIndices not label
+      !n1 = UV.length idx1
+      !n2 = UV.length idx2
+      !df = UV.length label - 2
+      !(m1,v1) = vecFastMeanVar $! V.unsafeBackpermute expData (UV.convert idx1)
+      !(m2,v2) = vecFastMeanVar $! V.unsafeBackpermute expData (UV.convert idx2)
+      !num = UV.zipWith (-) m1 m2
+      !den = UV.map ((* (sqrt $ 1 / fromIntegral n1 + 1 / fromIntegral n2 ))
+                     . sqrt . (/ fromIntegral df)) $
+             UV.zipWith (+)
+             (UV.map (* (fromIntegral $ n1-1)) v1)
+             (UV.map (* (fromIntegral $ n2-1)) v2)
+  in UV.zipWith (/) num den
+
+
+ttestTwoTail :: V.Vector (UV.Vector FloatType)
+             -> UV.Vector Bool
+             -> UV.Vector FloatType
+ttestTwoTail !expData !label =
+  let !dis = studentT $ fromIntegral $ UV.length label -2
+      !ts = UV.map abs $ tstat expData label
+  in UV.map (\t -> 2 * cumulative dis (negate t)) ts
+
+ttestLT :: V.Vector (UV.Vector FloatType)
+             -> UV.Vector Bool
+             -> UV.Vector FloatType
+ttestLT !expData !label =
+  let !dis = studentT $ fromIntegral $ UV.length label -2
+  in UV.map (cumulative dis) $ tstat expData label
+
+ttestRT :: V.Vector (UV.Vector FloatType)
+             -> UV.Vector Bool
+             -> UV.Vector FloatType
+ttestRT !expData !label = UV.map (1 -) $
+                          ttestLT expData label
+
+
+permFDR :: Seed
+        -> Int
+        -> V.Vector (UV.Vector FloatType)
+        -> UV.Vector Bool
+        -> UV.Vector (FloatType,FloatType)
+permFDR seed n expData label =
+  let ts = tstat expData label
+  in undefined      

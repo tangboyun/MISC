ca5d040f3b90f3cf875c0d101eda33b640c059ca
Boyun Tang <tangboyun@hotmail.com>
2012-07-27 11:58:12 +0800

    add permFDR

--- PValueAdj/PAdj.hs
+++ PValueAdj/PAdj.hs
@@ -11,23 +11,28 @@
 --             Journal of the Royal Statistical Society, Series B, 64: 479-498.
 --
 --             Mitchell Guttman (2011) lincRNAs act in the circuitry controlling pluripotency and differentiation 
---             Nature 477, 295–300 
+--             Nature 477, 295–300
+--
+--             Finding consistent patterns: a nonparametric approach for identifying differential expression in RNA-Seq data
+--             Jun Li, Robert Tibshirani
+--
 -----------------------------------------------------------------------------
-
 module PValueAdj where
 
+import           Control.Monad.ST
+import           Control.Parallel.Strategies
 import           Data.Function
 import           Data.List
-import qualified Data.Vector.Unboxed as UV
 import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as UV
+import           Debug.Trace
+import           Statistics.Basic
+import           Statistics.Sampling
+import           Statistics.Distribution
 import           Statistics.Distribution
 import           Statistics.Distribution.ChiSquared
-import System.Random.MWC
-import Control.Monad.ST
-import Debug.Trace
-import Statistics.Basic
-import Statistics.Distribution.StudentT
-import Statistics.Distribution
+import           Statistics.Distribution.StudentT
+import           System.Random.MWC
 
 type FloatType = Double
 
@@ -52,6 +57,26 @@ rankAll !vec =
      sortBy (compare `on` (vec `at`)) [0..n-1]
   where 
     at = UV.unsafeIndex
+
+{-# INLINE rankAll' #-}
+-- | 返回rank,即为原数组中小于该值的个数,不处理相同值
+rankAll' :: UV.Vector FloatType -> UV.Vector FloatType  
+rankAll' !vec =
+  let !n = UV.length vec
+  in UV.fromList $
+     map (fromIntegral . snd) $
+     sortBy (compare `on` fst) $ fst $
+     foldl' (\(acc,count) ls ->
+              let !acc' = map (\e -> (e,count)) ls ++ acc
+                  !count' = count + length ls
+              in (acc',count')
+              ) ([],0) $
+     groupBy ((==) `on` (vec `at`)) $ 
+     sortBy (compare `on` (vec `at`)) [0..n-1]
+  where 
+    at = UV.unsafeIndex
+
+
 
 {-# INLINE bhFDR #-}
 bhFDR :: UV.Vector FloatType -> UV.Vector FloatType
@@ -78,45 +103,45 @@ bonferroni !pVec = UV.map (\e -> if e > 1 then 1 else e) $
 
 
 -- | q-value for pFDR
-qValue :: Seed                -- ^ initial random seed
-       -> UV.Vector FloatType -- ^ range used to find the optimal pi0
-       -> UV.Vector FloatType -- ^ input p-value
-       -> (UV.Vector FloatType,Seed)
-qValue seed lambda' pVec =
-  let m = UV.length pVec
-      n = 100
-      l = UV.length lambda
-      f ls ps = UV.map
-                (\lam ->
-                  (fromIntegral $ UV.length $
-                   UV.findIndices (>= lam) ps) /
-                  fromIntegral m / ( 1 - lam)
-                ) ls
-      validIdx = UV.findIndices (\e -> e > 0 && e < 1) $ f lambda' pVec
-      lambda = UV.unsafeBackpermute lambda' validIdx
-      pi0s = f lambda pVec
-      minPi0 = minimum $
-               UV.toList $ UV.filter (\e -> e >= 0 && e < 1) pi0s
-      (bootMSE,seed') =
-        foldl'
-        (\(acc,s) _ ->
-          let (bootIdx,s') = runST $ do
-                gen <- initialize $ fromSeed s
-                idxs <- UV.replicateM m $
-                        uniformR (0,l-1) gen
-                s'' <- save gen
-                return $ (idxs,s'')
-              -- sampling with replacement
-              bootPVec = UV.unsafeBackpermute pVec bootIdx
-              pi0Boot = f lambda bootPVec
-              acc' = UV.zipWith (+) acc $
-                     UV.map ((^^(2::Int)).(\p -> p - minPi0)) pi0Boot 
-          in (acc',s') 
-        ) (UV.replicate l 0,seed) [0 .. n-1]
-      piIdx = minimumBy (compare `on` (bootMSE `UV.unsafeIndex`)) [0..l-1]
-      myPi = min 1 $ pi0s `UV.unsafeIndex` piIdx                                 
-      v = rankAll pVec
-  in trace (show myPi ++ show pi0s) $ (UV.map ((*myPi).(* fromIntegral m)) $ UV.zipWith (/) pVec v,seed')
+-- qValue :: Seed                -- ^ initial random seed
+--        -> UV.Vector FloatType -- ^ range used to find the optimal pi0
+--        -> UV.Vector FloatType -- ^ input p-value
+--        -> (UV.Vector FloatType,Seed)
+-- qValue seed lambda' pVec =
+--   let m = UV.length pVec
+--       n = 100
+--       l = UV.length lambda
+--       f ls ps = UV.map
+--                 (\lam ->
+--                   (fromIntegral $ UV.length $
+--                    UV.findIndices (>= lam) ps) /
+--                   fromIntegral m / ( 1 - lam)
+--                 ) ls
+--       validIdx = UV.findIndices (\e -> e > 0 && e < 1) $ f lambda' pVec
+--       lambda = UV.unsafeBackpermute lambda' validIdx
+--       pi0s = f lambda pVec
+--       minPi0 = minimum $
+--                UV.toList $ UV.filter (\e -> e >= 0 && e < 1) pi0s
+--       (bootMSE,seed') =
+--         foldl'
+--         (\(acc,s) _ ->
+--           let (bootIdx,s') = runST $ do
+--                 gen <- initialize $ fromSeed s
+--                 idxs <- UV.replicateM m $
+--                         uniformR (0,l-1) gen
+--                 s'' <- save gen
+--                 return $ (idxs,s'')
+--               -- sampling with replacement
+--               bootPVec = UV.unsafeBackpermute pVec bootIdx
+--               pi0Boot = f lambda bootPVec
+--               acc' = UV.zipWith (+) acc $
+--                      UV.map ((^^(2::Int)).(\p -> p - minPi0)) pi0Boot 
+--           in (acc',s') 
+--         ) (UV.replicate l 0,seed) [0 .. n-1]
+--       piIdx = minimumBy (compare `on` (bootMSE `UV.unsafeIndex`)) [0..l-1]
+--       myPi = min 1 $ pi0s `UV.unsafeIndex` piIdx                                 
+--       v = rankAll pVec
+--   in trace (show myPi ++ show pi0s) $ (UV.map ((*myPi).(* fromIntegral m)) $ UV.zipWith (/) pVec v,seed')
 
 
 tstat :: V.Vector (UV.Vector FloatType)
@@ -161,11 +186,70 @@ ttestRT !expData !label = UV.map (1 -) $
                           ttestLT expData label
 
 
+median :: UV.Vector FloatType -> FloatType
+median vec =
+  let l = UV.length vec
+      vec' = UV.fromList $ sort $ UV.toList vec
+  in if odd l
+     then vec' `UV.unsafeIndex` (l `div` 2)
+     else let l2 = l `div` 2
+              l1 = l2 - 1
+          in 0.5 * (vec' `UV.unsafeIndex` l1 +
+                    vec' `UV.unsafeIndex` l2)
+    
 permFDR :: Seed
         -> Int
+        -> (V.Vector (UV.Vector FloatType)
+           -> UV.Vector Bool
+           -> UV.Vector FloatType)
         -> V.Vector (UV.Vector FloatType)
         -> UV.Vector Bool
-        -> UV.Vector (FloatType,FloatType)
-permFDR seed n expData label =
-  let ts = tstat expData label
-  in undefined      
+        -> (UV.Vector FloatType,Seed)
+permFDR seed n f expData label =
+  let !ts = f expData label
+      !rs = rankAll' $ UV.map (negate . abs) ts
+      nT = UV.length ts
+      idx1 = UV.findIndices id label
+      l = UV.length label
+      ls = [0..l-1]
+      l1 = UV.length idx1
+      (rndLabels,seed') = if comb (fromIntegral l1) (fromIntegral l) < fromIntegral n -- 是否达到理论上限
+                          then let !rndLs = map (\idxs -> UV.fromList $ map (`elem` idxs) ls) $ combinations l1 ls
+                               in (rndLs,seed)
+                          else foldl'
+                               (\(acc,s) _ ->
+                                  let !(rndLabel,s') = shuffle s label
+                                  in (rndLabel:acc,s')
+                               ) ([],seed) [1..n]
+      !rndTs = parMap rseq (f expData) rndLabels             
+      !fdrs' = UV.imap (\i _ ->
+                         let t = abs $ ts `UV.unsafeIndex` i
+                             r = rs `UV.unsafeIndex` i
+                             vs = map (fromIntegral . UV.length .
+                                       UV.findIndices ((> t) . abs)) rndTs
+                             v = (\(a,leng) ->  a / leng) $!
+                                 foldl' (\(acc,len) e ->
+                                          let acc' = acc + e
+                                              len' = len + 1
+                                          in (acc',len')) (0,0) vs
+                         in v / r
+                       ) $ UV.replicate nT (0::FloatType)
+      !m = median $ foldr ((UV.++) `on` (UV.map abs)) UV.empty rndTs
+      pi = 2 * ( 1 / fromIntegral nT) * (fromIntegral $
+                                         UV.length $
+                                         UV.findIndices ((<= m) . abs) ts)
+  in (UV.map (* pi) fdrs',seed')
+
+
+
+comb :: FloatType -> FloatType -> Integer
+comb !a !b =
+  let c = b - a
+      num = foldl1' (+) $ map log [1..b]
+      den = foldl1' (+) $ map log $ [1..a] ++ [1..c]
+  in round $ exp $ num - den
+     
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _  = [[]]
+combinations n xs = [ y:ys | y:xs' <- tails xs
+                           , ys <- combinations (n-1) xs']

21b2d0c45c830734dfb2eedcaf823606afc494ed
Boyun Tang <tangboyun@hotmail.com>
2012-12-11 10:14:30 +0800

    简化shuffle与permute定义，修改pragma，用inlinable替代 SPECIALISE

--- Statistics/Sampling.hs
+++ Statistics/Sampling.hs
@@ -13,75 +13,51 @@
 -----------------------------------------------------------------------------
 module Statistics.Sampling 
        (
-         randomVecByFunc
-       , shuffle
+         shuffle
+       , permute
        )
        where
 
 import           Control.Monad
 import           Control.Monad.ST
-import qualified Data.Vector                 as V
 import qualified Data.Vector.Generic         as G
 import qualified Data.Vector.Generic.Mutable as GM
-import qualified Data.Vector.Unboxed         as UV
 import           System.Random.MWC
 
-{-# SPECIALIZE randomVecByFunc :: G.Vector v Double => Int -> Seed -> (forall s m . Gen s -> m Double) -> (v Double,Seed) #-}
-{-# SPECIALIZE randomVecByFunc :: G.Vector v Float => Int -> Seed -> (forall s m . Gen s -> m Float) -> (v Float,Seed) #-}
-{-# SPECIALIZE randomVecByFunc :: Int -> Seed -> (forall s m . Gen s -> m Double) -> (UV.Vector Double,Seed) #-}
-{-# SPECIALIZE randomVecByFunc :: Int -> Seed -> (forall s m . Gen s -> m Float) -> (UV.Vector Float,Seed) #-}
-randomVecByFunc :: (Floating a,G.Vector v a) => Int -> Seed -> (forall s m . Gen s -> m a) -> (v a,Seed)
-randomVecByFunc len s f = runST $ do
-  gen <- initialize $ fromSeed s
-  vec <- GM.new len
-  forM_ [0..len-1] $ \i -> do
-     v <- f gen
-     GM.unsafeWrite vec i v
-  s' <- save gen
-  vec' <- G.unsafeFreeze vec
-  return (vec',s')
 
-{-# SPECIALIZE shuffle :: Seed -> UV.Vector Int -> (UV.Vector Int,Seed) #-}
-{-# SPECIALIZE shuffle :: Seed -> V.Vector Int -> (V.Vector Int,Seed) #-}
-{-# SPECIALIZE shuffle :: G.Vector v Int => Seed -> v Int -> (v Int,Seed) #-}
-{-# SPECIALIZE shuffle :: G.Vector v Double => Seed -> v Double -> (v Double,Seed) #-}  
 shuffle :: G.Vector v a => Seed -> v a -> (v a,Seed)
 shuffle s v =
   runST $ do
     let len = G.length v
         n   = len-1
     mv <- GM.new len
-    gen <- initialize $ fromSeed s
+    gen <- restore s
     G.unsafeCopy mv v
     forM_ [0..n] $ \idx -> do
       idx' <- uniformR (idx,n) gen
-      val_i <- GM.read mv idx
-      val_j <- GM.read mv idx'
-      GM.write mv idx val_j
-      GM.write mv idx' val_i
+      GM.unsafeSwap mv idx idx'
     s' <- save gen
     v' <- G.unsafeFreeze mv
     return $ (v',s')
-
-permute :: G.Vector v a => Int -> Seed -> v a -> ([v a],Seed)
-permute n s vec =
+{-# INLINABLE shuffle #-}
+    
+permute :: G.Vector v a => Seed -> Int -> v a -> ([v a],Seed)
+permute s n vec =
   let l = G.length vec
       v = G.concat $ replicate n vec
       t = n * l
+      end = l - 1
   in runST $ do
     mv <- G.unsafeThaw v
     gen <- restore s
-    idxV <- UV.generateM t
-            (\idx ->
-              let (c,r) = idx `divMod` l
-                  i = c * l
-              in do
-                i' <- uniformR (r, l - 1) gen
-                return $! (i+i')
-            )
             
-    forM_ [0..t-1] $ \i -> 
-      GM.unsafeSwap mv i (UV.unsafeIndex idxV i)
+    forM_ [0..t-1] $ \idx -> do
+      let (c,r) = idx `divMod` l
+          i = c * l
+      i' <- uniformR (r,end) gen
+      GM.unsafeSwap mv idx (i+i') 
+
     v' <- G.unsafeFreeze mv
     s' <- save gen
     return (map ((\i -> G.unsafeSlice i l v').(*l)) [0..n-1] ,s')
+{-# INLINABLE permute #-}

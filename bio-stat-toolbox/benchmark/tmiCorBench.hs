e23b7eb27baed01d328911fc9e9e6a6fe996901b
Boyun Tang <tangboyun@hotmail.com>
2013-01-04 17:24:37 +0800

    first commit

---
+++ bio-stat-toolbox/benchmark/tmiCorBench.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE FlexibleContexts,BangPatterns #-}
+import Criterion.Main
+import System.Random.MWC
+import Bio.Statistics.Interaction.TMIcor
+import Control.Monad.ST (runST)
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector.Generic as GV
+
+testData :: Int -> Int -> (GData UV.Vector,UV.Vector Bool,Seed)
+testData n p =
+  runST $ do
+    gen <- create
+    datV <- uniformVector gen (n*p) 
+    bV <- uniformVector gen n
+    seed <- save gen
+    return (GD p n datV,bV,seed)
+    
+main :: IO ()
+main = do
+  let (gD,label,s) = testData 50 100
+      re = fst $ tmiCor s 100 10 gD label
+  print re
+  -- defaultMain
+  --  [bench "tmiCor" $ whnf f (testData 100 10000)]
+  -- where
+  --   f = \(gD,label,s) -> tmiCor s 1000 2000 gD label
--- bio-stat-toolbox/src/Bio/Statistics/Interaction/TMIcor.hs
+++ bio-stat-toolbox/src/Bio/Statistics/Interaction/TMIcor.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns,RankNTypes,FlexibleContexts,TypeFamilies #-}
+{-# LANGUAGE BangPatterns,RankNTypes,FlexibleContexts,TypeFamilies,GADTs #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module : 
@@ -13,9 +13,9 @@
 --
 -----------------------------------------------------------------------------
 
-module TMIcor
+module Bio.Statistics.Interaction.TMIcor
        (
-         Result(..)
+         Result
        , GData(..)
        , tmiCor
        )
@@ -32,27 +32,27 @@ import qualified Data.HashTable.ST.Basic as B
 import qualified Data.Heap as Heap
 import           Data.List
 import           Data.Maybe
-import           Data.Ord
 import qualified Data.Vector as V
 import qualified Data.Vector.Generic as GV
 import qualified Data.Vector.Generic.Mutable as GMV
 import qualified Data.Vector.Unboxed as UV
-import qualified Data.Vector.Unboxed.Mutable as UMV
 import           GHC.Conc (numCapabilities)
 import           Statistics.Sample
 import           System.Random.MWC
+import           Bio.Statistics.Util
+import           Debug.Trace (trace)
 
-
-newtype TmiCor = TmiCor (Int,Int,Double,Double)
 type HashTable s k v = B.HashTable s k v
 -- | Result: GeneIdx1, GeneIdx2, TMIcor-value, p-value, FDR
 type Result = UV.Vector (Int,Int,Double,Double,Double)
 
-data GData = GD
-  { pVariable :: {-# UNPACK #-} !Int -- p >> n
-  , nSample :: {-# UNPACK #-} !Int
-  , _data :: GV.Vector v Double => v Double
-  }
+data GData v where
+  GD :: GV.Vector v Double =>
+        { pVariable :: {-# UNPACK #-} !Int -- p >> n
+        , nSample :: {-# UNPACK #-} !Int
+        , dat :: v Double
+        } -> GData v
+
 
 newtype TCor = TCor
   { unTCor :: ((Int,Int),Double) } deriving (Eq)
@@ -74,27 +74,26 @@ kLargest k vVec label
   | GV.length (V.unsafeIndex vVec 0) /= GV.length label =
     error "firstK: sample num /= label length"
   | otherwise =
-    let n = GV.length label
-        p = V.length vVec
+    let p = V.length vVec
     in
      go 0 Heap.empty $
      map
-     (\ p@(i, j) ->
+     (\ idx@(i, j) ->
        let v = tCorStded (V.unsafeIndex vVec i) (V.unsafeIndex vVec j)
                label
-       in TCor (p, v))
+       in TCor (idx, v))
      [(i, j) | i <- [0 .. p - 1], j <- [i + 1 .. p - 1]]
   where go _ h [] = map unTCor $ toList h
         go acc h (x : xs)
           | acc < k = go (acc + 1) (Heap.insert x h) xs
           | otherwise = go (acc + 1) (Heap.insert x $ Heap.deleteMin h) xs
     
-tmiCor :: (GV.Vector v Bool)
+tmiCor :: (GV.Vector v1 Double,GV.Vector v2 Bool,v1~v2)
        => Seed -- ^ initial random seed
        -> Int  -- ^ repeat times for permutation
        -> Int  -- ^ first k gene pairs
-       -> GData -- ^ vector of gene chip
-       -> v Bool
+       -> GData v1 -- ^ vector of gene chip
+       -> v2 Bool
        -> (Result,Seed)
 {-# INLINABLE tmiCor #-}       
 tmiCor seed nPerm kPairs (GD p n _data) label' =
@@ -102,9 +101,9 @@ tmiCor seed nPerm kPairs (GD p n _data) label' =
       toSlices v = map ((\beg -> GV.slice beg n v).(*n)) [0..p-1]
       {-# INLINE toSlices #-}
       n1 = GV.foldl' (\acc e -> if e then acc + 1 else acc) 0 label'
-      label = GV.replicate n1 True UV.++ 
+      label = GV.replicate n1 True GV.++ 
               GV.replicate (GV.length label' - n1) False
-      vec = UV.concat $
+      vec = GV.concat $
             map
             (\v ->
               let (v1,v2) = ipartition' v label'
@@ -132,7 +131,7 @@ tmiCor seed nPerm kPairs (GD p n _data) label' =
        let vs = sort $
                 withStrategy
                 (parListChunk (ceiling $
-                               fromIntegral nPerm /
+                               (fromIntegral nPerm :: Double) /
                                fromIntegral numCapabilities) rdeepseq) $
                 map
                 (abs . tCor (V.unsafeIndex vVec i)
@@ -147,15 +146,16 @@ tmiCor seed nPerm kPairs (GD p n _data) label' =
            fromIntegral nPerm
          collect umv tCorVec vs'
      uv <- UV.unsafeFreeze umv
-     let fdrVec = UV.map (\e -> fromIntegral e / fromIntegral (kPairs*nPerm)) $
+     let fdrVec = trace (show uv ) $
+                  UV.imap (\i e -> fromIntegral e / fromIntegral ((i+1)*nPerm)) $
                   UV.postscanr' (+) 0 uv      
      result <- liftM UV.reverse $ UV.generateM kPairs $ \k -> 
        let idx@(i,j) = UV.unsafeIndex gpVec k
            cor = UV.unsafeIndex tCorVec k
            fdr = UV.unsafeIndex fdrVec k
        in do
-        p <- liftM fromJust $ H.lookup pTable idx 
-        return (i,j,cor,p,fdr)
+        pValue <- liftM fromJust $ H.lookup pTable idx 
+        return (i,j,cor,pValue,fdr)
      return (result,seed')
 
      
@@ -199,29 +199,6 @@ tCor vi vj label =
   in atanh (pcc v1i v1j) -
      atanh (pcc v2i v2j)
         
-pcc :: (GV.Vector v1 Double
-      ,GV.Vector v2 Double
-      ,v1 ~ v2)
-    => v1 Double
-    -> v2 Double
-    -> Double
-{-# INLINABLE pcc #-}    
-pcc v1 v2 | GV.length v1 == GV.length v2 =
-  let m1 = mean v1
-      m2 = mean v2
-  in (\(num,res1,res2) ->
-       num / sqrt (res1*res2)) $
-     GV.ifoldl'
-     (\(num,res1,res2) i e1 ->
-       let e2 = GV.unsafeIndex v2 i
-           r1 = e1-m1
-           r2 = e2-m2
-           num' = num+r1*r2
-           res1' = res1+r1*r1
-           res2' = res2+r2*r2
-       in (num',res1',res2')
-     ) (0,0,0) v1
-          | otherwise = error "pcc: input vec did not have equal length."
 
 
 pccStded :: (GV.Vector v1 Double
@@ -236,62 +213,3 @@ pccStded v1 v2 | GV.length v1 == GV.length v2 =
   GV.foldl1' (+) $ GV.zipWith (*) v1 v2
                | otherwise = error "pccStded: input vec did not have equal length."
             
-ipartition' :: (GV.Vector v1 a
-              ,GV.Vector v2 Bool) --,v1 ~ v2)
-            => v1 a -> v2 Bool
-            -> (v1 a, v1 a)
-{-# INLINABLE ipartition' #-}            
-ipartition' vec boolVec | GV.length vec == GV.length boolVec =
-  let n = GV.length vec
-  in runST $ do
-    mv <- GMV.unsafeNew n
-    (_,l,_) <- GV.foldM'
-               (\(idx,i,j) e ->
-                 let !idx' = idx + 1
-                 in if e
-                    then let !i' = i + 1
-                         in GMV.unsafeWrite mv i (GV.unsafeIndex vec idx) >>
-                            return (idx',i',j)
-                    else let !j' = j - 1
-                         in GMV.unsafeWrite mv j (GV.unsafeIndex vec idx) >>
-                            return (idx',i,j')
-               ) (0,0,n-1) boolVec
-    v1 <- GV.unsafeFreeze $ GMV.slice 0 l mv
-    v2 <- GV.unsafeFreeze $ GMV.slice l (n-l) mv
-    return (v1,v2)
-                        | otherwise = error "ipartition': input vec did not have equal length."
-                                      
-ipartition :: (GV.Vector v1 a
-             ,GV.Vector v2 Bool) --v1 ~ v2)
-           => v1 a -> v2 Bool
-           -> (v1 a, v1 a)
-{-# INLINABLE ipartition #-}           
-ipartition vec boolVec =
-  let (v1,v2) = ipartition' vec boolVec
-  in runST $ do
-    mv2 <- GV.unsafeThaw v2
-    GMV.reverse mv2
-    v2' <- GV.unsafeFreeze mv2
-    return (v1,v2')
-
-permute :: GV.Vector v a => Seed -> Int -> v a -> ([v a],Seed)
-{-# INLINABLE permute #-}
-permute s n vec =
-  let l = GV.length vec
-      v = GV.concat $ replicate n vec
-      t = n * l
-      end = l - 1
-  in runST $ do
-    mv <- GV.unsafeThaw v
-    gen <- restore s
-            
-    forM_ [0..t-1] $ \idx -> do
-      let (c,r) = idx `divMod` l
-          i = c * l
-      i' <- uniformR (r,end) gen
-      GMV.unsafeSwap mv idx (i+i') 
-
-    v' <- GV.unsafeFreeze mv
-    s' <- save gen
-    return (map ((\i -> GV.unsafeSlice i l v').(*l)) [0..n-1] ,s')
-
